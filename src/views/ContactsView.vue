32849b08db024e355393ac2f26e83ffce178cdf4
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2023-04-04 14:56:44 +0200

    Add working vue cloudflare verification

---
+++ src/views/ContactsView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+      <TurnstileComponent :sitekey="sitekey" @verify="verify"  />
+    </div>
+    
+    <div id="#container"></div>
+</template>
+  
+<script lang="ts">
+    import { defineComponent, ref, onMounted } from 'vue'
+    import TurnstileComponent from '@astrianz/cfturnstile-vue3'
+
+    export default defineComponent({
+        name: 'Contacts',
+        components: {
+            TurnstileComponent
+        },
+        setup() {
+            const sitekey = ref<string>('0x4AAAAAAADu0tdEcdycayFa');
+            const verify = (token: string): void => {
+                console.log(`Turnstile token: ${token}`)
+            }
+            onMounted(() => {
+                console.log("Turnstile Mounted")
+            });
+            return {
+                sitekey,
+                verify
+            }
+        }
+    })
+</script>
